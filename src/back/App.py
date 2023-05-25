702f0336520aa4dcf23e881f21f97077deaf6293
Tiago Silvestre <tiago.silvestre@ua.pt>
2023-05-25 23:51:33 +0100

    updated backend

--- src/back/App.py
+++ src/back/App.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import queries
 app = Flask(__name__)
+
+databaseInteraction = queries.DatabaseInteraction()
+
 
 @app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
+
+@app.route('/users')
+def get_users():
+    print(databaseInteraction.get_users())
+    return databaseInteraction.get_users()
 
 
 if __name__ == "__main__":
---
+++ src/back/queries.py
@@ -0,0 +1,43 @@
+import pyodbc
+import time
+
+config = {
+    'Driver': '{ODBC Driver 17 for SQL Server}',
+    'Server': 'localhost',
+    'Database': 'WasteNot_Warning',
+    'UID': 'sa',
+    'PWD': 'StrongP@ssw0rd123'
+}
+
+class DatabaseInteraction:
+    def __init__(self):
+        self.cursor = None
+        #try:
+        #    conn = self.establish_connection_with_retry()
+        #    self.cursor = conn.cursor()
+        #except Exception as e:
+        #    print(f"An error occurred: {str(e)}")
+
+    def establish_connection_with_retry(self):
+        max_retries = 2  # Maximum number of retries
+        retry_interval = 5  # Interval between retries (in seconds)
+
+        for attempt in range(1, max_retries + 1):
+            try:
+                # Attempt to establish the connection
+                conn = pyodbc.connect(**config)
+                print("Connection established")
+                return conn
+            except pyodbc.Error as err:
+                print(f"Error: {str(err)}")
+
+                print(f"Connection attempt {attempt} failed. Retrying in {retry_interval} seconds...")
+                time.sleep(retry_interval)
+
+        # Maximum retries exceeded, raise an exception
+        raise Exception("Unable to establish a connection with the SQL Server.")
+
+    def get_users(self):
+        if self.cursor:
+            self.cursor.execute("SELECT * FROM UTILIZADOR")
+            return self.cursor.fetchall()
