a4895ef6be9a1752aa283b6e2ca07e12173a6f4f
Tiago Silvestre <tiago.silvestre@ua.pt>
2023-05-28 23:19:55 +0100

    removed unused code from login page

--- src/front/src/router/index.ts
+++ src/front/src/router/index.ts
@@ -183,16 +183,6 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import('../pages/auth/login/Login.vue'),
       },
       {
-        name: 'signup',
-        path: 'signup',
-        component: () => import('../pages/auth/signup/Signup.vue'),
-      },
-      {
-        name: 'recover-password',
-        path: 'recover-password',
-        component: () => import('../pages/auth/recover-password/RecoverPassword.vue'),
-      },
-      {
         path: '',
         redirect: { name: 'login' },
       },
